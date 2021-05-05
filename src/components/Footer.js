1e725f3d606f8b6c8e99bd125ad1da9d758b3c81
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-04 23:08:47 -0400

    boilerplate

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+    Hello World again again.
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+  return (
+    <div>
+    Hello World again.
+    </div>
+  );
+}
+
+export default Project;
