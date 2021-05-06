ebd2934ae9292a6fa7ba26ecb1ddde034e25e5d1
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-06 17:52:56 -0400

    reusable component for projects

--- src/components/ProjectWraper/index.js
+++ src/components/ProjectWraper/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Project from '../Projects/index'
 import "./style.css";
+import projects from '../../projects.json'
 
 
 function ProjectWrapper() {
   return (
     <div id="project-container">
-        <Project/>
+      
+        <Project
+        title={projects[0].title}
+        url={projects[0].url}
+        altTop={projects[0].altTop}
+        imageTop={projects[0].imageTop}
+        altBottom={projects[0].altBottom}
+        imageBottom={projects[0].imageBottom}
+        git={projects[0].git}
+        />
+
     </div>
   );
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,19 +2,28 @@ import React from "react";
 import "./style.css";
 
 
-function Project() {
+function Project(props) {
   return (
     <div id="project">
 
+
             <div id="img-container">
+                <a className="wrapper" target="_blank" rel="noopener noreferrer" href={props.url}>
+                    <img alt={props.altTop} src={props.imageTop}/>
+                    <img alt={props.altBottom} src={props.imageBottom} className="img-top"/>
+                </a>
+                <div>{props.title}</div><a target="_blank" rel="noopener noreferrer" href={props.git}>github</a>
+            </div>
+
+            {/* <div id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://petermodavis.github.io/provisions/">
                     <img alt="provisions-project" src="images/provisions.png"/>
                     <img alt="screenshot" src="images/provisions-screenshot.png" className="img-top"/>
                 </a>
                 <div>provisions</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/provisions">github</a>
-            </div>
+            </div> */}
 
-            <div id="img-container">
+            {/* <div id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://jareddeuriarte.github.io/weather-dashboard/">
                     <img alt="weather-dashboard" src="images/weather-dashboard.png"/>
                     <img alt="screenshot" src="images/weather-dash-screenshot.png" className="img-top"/>
@@ -28,7 +37,7 @@ function Project() {
                     <img alt="screenshot" src="images/password-generator-screenshot.png" className="img-top"/>
                 </a>
                 <div>password generator</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/password-generator">github</a>
-            </div>
+            </div> */}
 
         
 
