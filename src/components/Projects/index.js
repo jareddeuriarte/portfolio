6b183d01611db4021fbe75f24c1cc0b35b05dd3a
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-05 22:05:30 -0400

    partial style on projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./style.css";
+
 
 function Project() {
   return (
@@ -7,45 +9,29 @@ function Project() {
 
             <div className="four columns" id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://petermodavis.github.io/provisions/">
-                    <img alt="provisions-project" src="./assets/images/provisions.png">
-                    <img alt="screenshot" src="./assets/images/provisions-screenshot.png" className="img-top">
+                    <img alt="provisions-project" src="images/provisions.png"/>
+                    <img alt="screenshot" src="images/provisions-screenshot.png" className="img-top"/>
                 </a>
                 <div>provisions</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/provisions">github</a>
             </div>
 
             <div className="four columns" id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://jareddeuriarte.github.io/weather-dashboard/">
-                    <img alt="weather-dashboard" src="./assets/images/weather-dashboard.png">
-                    <img alt="screenshot" src="./assets/images/weather-dash-screenshot.png" className="img-top">
+                    <img alt="weather-dashboard" src="images/weather-dashboard.png"/>
+                    <img alt="screenshot" src="images/weather-dash-screenshot.png" className="img-top"/>
                 </a>
                 <div>weather dashboard</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/weather-dashboard">github</a>
             </div>
 
             <div className="four columns" id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://jareddeuriarte.github.io/password-generator/">
-                    <img alt="pasword-generator" src="./assets/images/password-generator.png">
-                    <img alt="screenshot" src="./assets/images/password-generator-screenshot.png" className="img-top">
+                    <img alt="pasword-generator" src="images/password-generator.png"/>
+                    <img alt="screenshot" src="images/password-generator-screenshot.png" className="img-top"/>
                 </a>
                 <div>password generator</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/password-generator">github</a>
             </div>
 
         
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
 
 
 
