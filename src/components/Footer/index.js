65ed605494bda37d4731ad7ac0288f6c159cfc2e
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-05 22:17:13 -0400

    footer scomponent, no style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./style.css";
+
 
 function Footer() {
   return (
     <div>
-    Hello World again again.
+        <div className="row contact" id="contact">
+            <div className="four columns">
+                <h6>954-751-9104‬</h6>
+            </div>
+            <div className="four columns">
+                <h6><a href="deuriartejared@gmail.com">email</a></h6>
+            </div>
+
+            <div className="four columns">
+                <h6><a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/jareddeuri">linkedin</a></h6>
+            </div>
+        </div>
+    
+
+
+
+
+
     </div>
   );
 }
