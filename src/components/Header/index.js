047ff26052d2ab383d77324a360590fafa0e0e82
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-06 14:29:24 -0400

    header card flexing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,13 +6,17 @@ function Header() {
   return (
     
     <div id='header'>
-      <img id='pic-of-me' alt="me!" src="images/pic-of-me.jpg"></img>
+      <div id='bio'>
       <h1 id="name">Jared de Uriarte</h1>
       <h2 id="web-dev">web developer</h2>
       <p id="bio">Drawn to that magic where creativity and problem solving meet—coding.
       If I'm not in front of a screen and on the internet, I'm either on my bike, attending to my plants,
       or listening to <a className="breakFree" target="_blank" rel="noopener noreferrer"
           href="https://www.youtube.com/watch?v=FN5EJz9lMuM"><span>'Break Free'</span></a> by Ariana Grande.</p>
+    </div>
+
+      <img id='pic-of-me' alt="me!" src="images/pic-of-me.jpg"></img>
+
     </div>
   );
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,30 +4,9 @@ import "./style.css";
 
 function Project() {
   return (
-    <div>
+    <div id="project">
 
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-            <div className="four columns" id="img-container">
+            <div id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://petermodavis.github.io/provisions/">
                     <img alt="provisions-project" src="images/provisions.png"/>
                     <img alt="screenshot" src="images/provisions-screenshot.png" className="img-top"/>
@@ -35,7 +14,7 @@ function Project() {
                 <div>provisions</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/provisions">github</a>
             </div>
 
-            <div className="four columns" id="img-container">
+            <div id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://jareddeuriarte.github.io/weather-dashboard/">
                     <img alt="weather-dashboard" src="images/weather-dashboard.png"/>
                     <img alt="screenshot" src="images/weather-dash-screenshot.png" className="img-top"/>
@@ -43,7 +22,7 @@ function Project() {
                 <div>weather dashboard</div><a target="_blank" rel="noopener noreferrer" href="https://github.com/jareddeuriarte/weather-dashboard">github</a>
             </div>
 
-            <div className="four columns" id="img-container">
+            <div id="img-container">
                 <a className="wrapper" target="_blank" rel="noopener noreferrer" href="https://jareddeuriarte.github.io/password-generator/">
                     <img alt="pasword-generator" src="images/password-generator.png"/>
                     <img alt="screenshot" src="images/password-generator-screenshot.png" className="img-top"/>
