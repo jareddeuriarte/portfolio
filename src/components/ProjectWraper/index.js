c68174ab9f802cfa456f700970b9c131cfe27c86
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-06 20:04:29 -0400

    tweaked background colors

--- src/components/ProjectWraper/index.js
+++ src/components/ProjectWraper/index.js
@@ -19,37 +19,7 @@ function ProjectWrapper() {
         git={project.git}
         />
       ))}
-
-      {/* <Project
-        title={projects[0].title}
-        url={projects[0].url}
-        altTop={projects[0].altTop}
-        imageTop={projects[0].imageTop}
-        altBottom={projects[0].altBottom}
-        imageBottom={projects[0].imageBottom}
-        git={projects[0].git}
-      />
-
-      <Project
-        title={projects[1].title}
-        url={projects[1].url}
-        altTop={projects[1].altTop}
-        imageTop={projects[1].imageTop}
-        altBottom={projects[1].altBottom}
-        imageBottom={projects[1].imageBottom}
-        git={projects[1].git}
-      />
-
-      <Project
-        title={projects[2].title}
-        url={projects[2].url}
-        altTop={projects[2].altTop}
-        imageTop={projects[2].imageTop}
-        altBottom={projects[2].altBottom}
-        imageBottom={projects[2].imageBottom}
-        git={projects[2].git}
-      /> */}
-
+      
     </div>
   );
 }
