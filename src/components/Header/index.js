883efc1241cbd2aad55e85025751b194e82f6574
Jared de Uriarte <jared.deuri@gmail.com>
2021-05-06 23:22:46 -0400

    fixed github icon a tag bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,9 +13,9 @@ function Header() {
       If I'm not in front of a screen and on the internet, I'm either on my bike, attending to my plants,
       or listening to<a className="breakFree" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=FN5EJz9lMuM"> 'Break Free' </a>by Ariana Grande.</span></p>
       
-      
+      <span>
       <a target="_blank" rel="noopener noreferrer" href='https://github.com/jareddeuriarte'><i className="fa">&#xf09b;</i></a>
-      
+      </span>
     
     
     
